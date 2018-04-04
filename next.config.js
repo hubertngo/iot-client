5e9c16c8b2f60286c149d413c403d46f816ac493
Trần Đức Tiến <ductienas@gmail.com>
2018-04-04 19:53:20 +0700

    add clear cache webpack loader

--- next.config.js
+++ next.config.js
@@ -6,8 +6,16 @@
 * Created: 2018-02-12 01:16:17
 *------------------------------------------------------- */
 
+function disableCacheDirectory(config) {
+	config.module.rules
+		.filter(({ loader }) => loader === 'babel-loader')
+		.map(l => (l.options.cacheDirectory = false)); // eslint-disable-line
+}
+
 module.exports = {
 	webpack: (config) => {
+		disableCacheDirectory(config);
+
 		config.module.rules.push(
 			{
 				test: /\.less$/,
