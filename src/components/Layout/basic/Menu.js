78ff05ca77dc31aa593c479b624655fdc0a6d5eb
Ngo An Ninh <ninh.uit@gmail.com>
2018-05-30 22:19:32 +0700

    update tree view for menu

--- src/components/Layout/basic/Menu.js
+++ src/components/Layout/basic/Menu.js
@@ -13,7 +13,7 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'next/router';
 import Link from 'next/link';
 import { injectIntl, intlShape } from 'react-intl';
-import { Menu, Checkbox, Row, Col } from 'antd';
+import { Menu, Checkbox, Row, Col, Tree } from 'antd';
 // Styles
 import withStyles from 'src/theme/jss/withStyles';
 
@@ -26,9 +26,25 @@ import { getSensorList, selectSensors } from 'src/redux/actions/sensor';
 import AuthStorage from 'src/utils/AuthStorage';
 
 // Components
+const TreeNode = Tree.TreeNode;
 const SubMenu = Menu.SubMenu;
 const styleSheet = (theme) => ({
 });
+
+
+const treeData = [{
+	title: 'Sensor',
+	key: '10',
+	children: [
+		{ title: 'sensor1', key: 3 },
+		{ title: 'sensor2', key: 4 },
+		{ title: 'sensor3', key: 2 },
+		{ title: 'sensor4', key: 1 },
+		{ title: 'sensor5', key: 0 },
+	],
+},
+];
+
 
 function mapStateToProps(state) {
 	return {
@@ -63,6 +79,13 @@ export default class MenuComponent extends Component {
 		}).isRequired,
 	}
 
+	state = {
+		expandedKeys: ['10'],
+		autoExpandParent: true,
+		checkedKeys: [],
+		selectedKeys: [],
+	}
+
 	componentWillMount = () => {
 		this.props.action.getSensorList({
 			filter: {
@@ -81,33 +104,45 @@ export default class MenuComponent extends Component {
 		this.props.action.selectSensors(checkedValues);
 	}
 
+	onCheck = (checkedKeys) => {
+		this.props.action.selectSensors(checkedKeys);
+		this.setState({ checkedKeys });
+	}
+	onSelect = (selectedKeys, info) => {
+		console.log('onSelect', info.node.props.title);
+		this.setState({ selectedKeys });
+	}
+	renderTreeNodes = (data) => {
+		return data.map((item) => {
+			if (item.children) {
+				return (
+					<TreeNode title={item.title} key={item.key} dataRef={item}>
+						{this.renderTreeNodes(item.children)}
+					</TreeNode>
+				);
+			}
+			return <TreeNode {...item} />;
+		});
+	}
+
 	render() {
 		return (
 			<Checkbox.Group style={{ width: '100%' }} onChange={this.onChange}>
-				<Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline" style={{ color: '#fff' }}>
-					<SubMenu
-						key="sub1"
-						title={<span style={{ color: '#000000' }}>Sensor</span>}
-					>
-						<Menu.Item key="1">
-							<Checkbox value={0}>sensor5</Checkbox>
-						</Menu.Item>
-						<Menu.Item key="2">
-							<Checkbox value={1}>sensor4</Checkbox>
-						</Menu.Item>
-						<Menu.Item key="3">
-							<Checkbox value={2}>sensor3</Checkbox>
-						</Menu.Item>
-						<Menu.Item key="4">
-							<Checkbox value={4}>sensor2</Checkbox>
-						</Menu.Item>
-						<Menu.Item key="5">
-							<Checkbox value={3}>sensor1</Checkbox>
-						</Menu.Item>
-					</SubMenu>
-				</Menu>
+				<Tree
+					showLine
+					checkable
+					expandedKeys={this.state.expandedKeys}
+					autoExpandParent={this.state.autoExpandParent}
+					onCheck={this.onCheck}
+					checkedKeys={this.state.checkedKeys}
+					onSelect={this.onSelect}
+					selectedKeys={this.state.selectedKeys}
+				>
+					{this.renderTreeNodes(treeData)}
+				</Tree>
 			</Checkbox.Group>
 		);
 	}
 }
+
 
